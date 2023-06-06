0684f999422d66123c6cff08cb1b712e6e8bb5b7
Administrator <Administrator@PC065>
2023-06-06 21:40:33 +0900

    Feature: Reply, User Table Join 설정 #4

--- Repflix/src/main/java/repflix/entity/ReplyEntity.java
+++ Repflix/src/main/java/repflix/entity/ReplyEntity.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -25,10 +27,8 @@ public class ReplyEntity {
 	
 	@Column(nullable = false)
 	private String content;
-	
-	@Column(nullable = false)
-	private Integer userId;
-	
-	@Column(nullable = false)
-	private Integer listId;
+
+	@ManyToOne
+	@JoinColumn(name="USER_ID")
+	private UserEntity user;
 }
